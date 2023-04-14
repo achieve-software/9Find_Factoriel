730b2ddf5a1ab2538405873d61e8286eeff74314
Achieve-software <vtcbcdtyr@gmail.com>
2023-04-14 20:49:20 +0300

    fact

---
+++ index.js
@@ -0,0 +1,7 @@
+//Kullanıcıdan alınan sayının faktoriyelini alan program
+let factorial = Number(prompt("Enter a number"));
+let result = 1;
+for (let i = 1; i <= factorial; i++) {
+  result = result * i;
+}
+console.log(factorial + "'s factoriel is " + result);
